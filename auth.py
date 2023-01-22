930803e07fc26e5dddcc9c0dc49081f051e8e1b7
Jan Pedersen <jp@jp-embedded.com>
2023-01-22 19:42:39 +0100

    fix indent

--- auth.py
+++ auth.py
@@ -2,6 +2,6 @@ import teslapy
 with teslapy.Tesla(email=input('Enter e-mail: '), cache_file='/var/tmp/tesla_cron.json') as tesla:
     if not tesla.authorized:
         tesla.refresh_token(refresh_token=input('Enter SSO refresh token: '))
-        vehicles = tesla.vehicle_list()
-        print(vehicles[0])
+    vehicles = tesla.vehicle_list()
+    print(vehicles[0])
 
